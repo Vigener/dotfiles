6f8fdb0c04cdf4b45a36d6b02f6e386806b4ea5c
Mikoto Igarashi <s2310970@u.tsukuba.ac.jp>
2026-04-22 07:59:20 +0900

    chore(karabiner): コメントアウトされたウィンドウ操作のマッピングを削除

--- karabiner/index.ts
+++ karabiner/index.ts
@@ -254,14 +254,14 @@ writeToProfile(
             map("q", "left_control").to("w", "command"),
         ]),
 
-        rule(
-            "【WINDOW】Option + HJKL (Mac標準のウィンドウ/スペース操作へマッピング)",
-        ).manipulators([
-            map("j", "left_option").to("m", "command"), // 最小化 (Cmd+M)
-            map("k", "left_option").to("f", ["control", "command"]), // フルスクリーン (Ctrl+Cmd+F)
-            // map("h", "left_option").to("left_arrow", "control"), // 左のスペースへ移動 (Ctrl+←)
-            // map("l", "left_option").to("right_arrow", "control"), // 右のスペースへ移動 (Ctrl+→)
-        ]),
+        // rule(
+        //     "【WINDOW】Option + HJKL (Mac標準のウィンドウ/スペース操作へマッピング)",
+        // ).manipulators([
+        //     // map("j", "left_option").to("m", "command"), // 最小化 (Cmd+M)
+        //     // map("k", "left_option").to("f", ["control", "command"]), // フルスクリーン (Ctrl+Cmd+F)
+        //     // map("h", "left_option").to("left_arrow", "control"), // 左のスペースへ移動 (Ctrl+←)
+        //     // map("l", "left_option").to("right_arrow", "control"), // 右のスペースへ移動 (Ctrl+→)
+        // ]),
 
         // =====================================================================
         // [APP] かなコンビネーション (メディア操作・アプリ起動)
